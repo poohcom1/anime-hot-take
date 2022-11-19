0470720d9730c3397997c810a245a91c245ff975
poohcom1 <pooh677@gmail.com>
2022-11-19 16:04:44 +0700

    Added vercel adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import solid from "solid-start/vite";
+import vercel from "solid-start-vercel";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
     solid({
       ssr: false,
-      babel: (_, id) => ({
+      babel: (_: any, id: any) => ({
         plugins: [["solid-styled", { source: id }]],
       }),
+      adapter: vercel(),
     }),
   ],
 });
