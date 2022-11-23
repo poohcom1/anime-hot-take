e19b7cd92741f1dc17fb5c825384d631697586d5
poohcom1 <pooh677@gmail.com>
2022-11-23 21:14:40 +0700

    Updated env and fixed style on mobile

--- src/server/malClient.ts
+++ src/server/malClient.ts
@@ -14,7 +14,7 @@ export async function getMalClient(): Promise<Mal.MalAcount> {
     return accountCache;
   }
 
-  const auth = Mal.auth(import.meta.env.VITE_MAL_CLIENT_ID);
+  const auth = Mal.auth(process.env.MAL_CLIENT_ID);
   const account = await auth.guestLogin();
 
   accountCache = account;
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
 import solid from "solid-start/vite";
+// @ts-ignore
 import vercel from "solid-start-vercel";
 import { defineConfig } from "vite";
+import dotenv from "dotenv";
 
-export default defineConfig({
-  plugins: [
-    solid({
-      ssr: false,
-      babel: (_: any, id: any) => ({
-        plugins: [["solid-styled", { source: id }]],
+export default defineConfig(() => {
+  dotenv.config();
+
+  return {
+    plugins: [
+      solid({
+        ssr: false,
+        babel: (_: any, id: any) => ({
+          plugins: [["solid-styled", { source: id }]],
+        }),
+        adapter: vercel(),
       }),
-      adapter: vercel(),
-    }),
-  ],
+    ],
+  };
 });
